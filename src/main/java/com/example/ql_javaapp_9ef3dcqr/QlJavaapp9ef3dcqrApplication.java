c9c51449da86c77d27102d605557458a43d28f36
ldy827 <103937790+ldy827@users.noreply.github.com>
2022-06-27 19:06:26 +0900

    Add files via upload

---
+++ src/main/java/com/example/ql_javaapp_9ef3dcqr/QlJavaapp9ef3dcqrApplication.java
@@ -0,0 +1,13 @@
+package com.example.ql_javaapp_9ef3dcqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlJavaapp9ef3dcqrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlJavaapp9ef3dcqrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ql_javaapp_9ef3dcqr/QlJavaapp9ef3dcqrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ql_javaapp_9ef3dcqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlJavaapp9ef3dcqrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
