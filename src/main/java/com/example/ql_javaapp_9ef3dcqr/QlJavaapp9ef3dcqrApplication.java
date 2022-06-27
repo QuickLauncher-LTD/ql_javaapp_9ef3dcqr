d2766d83ca828caff745d4181a25ee499b4045a1
ldy827 <103937790+ldy827@users.noreply.github.com>
2022-06-27 19:15:41 +0900

    Update QlJavaapp9ef3dcqrApplication.java

--- src/main/java/com/example/ql_javaapp_9ef3dcqr/QlJavaapp9ef3dcqrApplication.java
+++ src/main/java/com/example/ql_javaapp_9ef3dcqr/QlJavaapp9ef3dcqrApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class QlJavaapp9ef3dcqrApplication {
 
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(QlJavaapp9ef3dcqrApplication.class, args);
     }
